4275a1c6cb90477a3a10a088edeec44fa8c1d2cb
Patryk Krajnik <patrykkrajnik@icloud.com>
2022-01-08 21:34:50 +0100

    Init of the project

---
+++ Universal-App/Universal-App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Universal-App
+//
+//  Created by Patryk Krajnik on 08/01/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
