f29032ec2bd319d9fe8db9bf1192caee15688be3
Patryk Krajnik <patrykkrajnik@icloud.com>
2022-01-09 19:23:26 +0100

    ViewControllers for radio player and webview created

---
+++ Universal-App/Universal-App/RadioPlayerViewController.swift
@@ -0,0 +1,19 @@
+//
+//  RadioPlayerViewController.swift
+//  Universal-App
+//
+//  Created by Patryk Krajnik on 09/01/2022.
+//
+
+import Foundation
+import UIKit
+
+final internal class RadioPlayerViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .red
+    }
+
+}
---
+++ Universal-App/Universal-App/WebviewViewController.swift
@@ -0,0 +1,19 @@
+//
+//  WebviewViewController.swift
+//  Universal-App
+//
+//  Created by Patryk Krajnik on 09/01/2022.
+//
+
+import Foundation
+import UIKit
+
+final internal class WebviewViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .green
+    }
+
+}
