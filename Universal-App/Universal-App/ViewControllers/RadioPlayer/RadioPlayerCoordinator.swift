8416b7cd3dcd32a100ebbe979bab0d76712fe9d5
Patryk Krajnik <patrykkrajnik@icloud.com>
2022-03-06 19:32:00 +0100

    ViewModel and Coordinator for RadioPlayer created

---
+++ Universal-App/Universal-App/ViewControllers/RadioPlayer/RadioPlayerCoordinator.swift
@@ -0,0 +1,12 @@
+//
+//  RadioPlayerCoordinator.swift
+//  Universal-App
+//
+//  Created by Patryk Krajnik on 06/03/2022.
+//
+
+import Foundation
+
+final internal class RadioPlayerCoordinator {
+    
+}
---
+++ Universal-App/Universal-App/ViewControllers/RadioPlayer/RadioPlayerViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  RadioPlayerViewModel.swift
+//  Universal-App
+//
+//  Created by Patryk Krajnik on 06/03/2022.
+//
+
+import Foundation
+
+final internal class RadioPlayerViewModel {
+    
+}
